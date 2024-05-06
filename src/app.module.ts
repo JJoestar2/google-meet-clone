f59790e590d5cc99e5fd997289243dd3ef3e4be7
Bohdan <gavriltsiv250@gmail.com>
2024-05-06 22:48:27 +0300

    fix: Added missing imports, installed validatior libs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { LoggerModule } from 'nestjs-pino';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RoomGateway } from './room/room.gateway';
 
 @Module({
-  imports: [],
+  imports: [
+    LoggerModule.forRoot(),
+    ConfigModule.forRoot(),
+  ],
   controllers: [AppController],
   providers: [AppService, RoomGateway],
 })
